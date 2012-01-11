8cbd21734a8fcee1ca0d83192d4c1a457ffbf6d9
Carel <careld@voxtelecom.com>
2012-01-11 15:31:56 +0200

    asd Signed-off-by: Carel <careld@voxtelecom.com>

--- Vox.Auditing/Properties/AssemblyInfo.cs
+++ Vox.Auditing/Properties/AssemblyInfo.cs
@@ -2,10 +2,12 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vox.Auditing")]
+
 [assembly: AssemblyDescription("This is For auditing perposes")]
 [assembly: AssemblyCompany("Vox")]
 [assembly: AssemblyProduct("Vox.Auditing")]
